9f99b8cb876a07aafd1ec4e5070be34f4c472d4d
Ariel Cardoso <arielvix@gmail.com>
2020-12-17 22:21:01 -0300

    Add reveal animation

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import formatCurrency from '../util';
+import Fade from 'react-reveal/Fade';
 
 export default class Cart extends Component {
   constructor(props) {
@@ -36,22 +37,24 @@ export default class Cart extends Component {
         </div>
         <div>
           <div className="cart">
-            <ul className="cart-items">
-              {cartItems.map(item => (
-                <li key={item._id} >
-                  <div>
-                    <img src={item.image} alt={item.title} />
-                  </div>
-                  <div>
-                    <div>{item.title}</div>
-                    <div className="right">
-                      {item.count} x {formatCurrency(item.price)}
-                      <button className="button" onClick={()=>this.props.removeFromCart(item)}>Remove</button>
+            <Fade left cascade={true}>
+              <ul className="cart-items">
+                {cartItems.map(item => (
+                  <li key={item._id} >
+                    <div>
+                      <img src={item.image} alt={item.title} />
                     </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
+                    <div>
+                      <div>{item.title}</div>
+                      <div className="right">
+                        {item.count} x {formatCurrency(item.price)}
+                        <button className="button" onClick={()=>this.props.removeFromCart(item)}>Remove</button>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </Fade>
           </div>
           {cartItems.length !== 0 && (
             <div>
@@ -69,27 +72,30 @@ export default class Cart extends Component {
                 </div>
               </div>
               { this.state.showCheckout && (
-                <div className='cart'>
-                  <form onSubmit={this.createOrder}>
-                    <ul className="form-container">
-                      <li>
-                        <label>Email</label>
-                        <input name="email" type="email" required onChange={this.handleInput}/>
-                      </li>
-                      <li>
-                        <label>Name</label>
-                        <input name="name" type="text" required onChange={this.handleInput}/>
-                      </li>
-                      <li>
-                        <label>Address</label>
-                        <input name="address" type="text" required onChange={this.handleInput}/>
-                      </li>
-                      <li>
-                        <button className="button-primary" type="submit" >Checkout</button>
-                      </li>
-                    </ul>
-                  </form>
-                </div>
+                <Fade right cascade >
+                  <div className='cart'>
+                    <form onSubmit={this.createOrder}>
+                      <ul className="form-container">
+                        <li>
+                          <label>Email</label>
+                          <input name="email" type="email" required onChange={this.handleInput}/>
+                        </li>
+                        <li>
+                          <label>Name</label>
+                          <input name="name" type="text" required onChange={this.handleInput}/>
+                        </li>
+                        <li>
+                          <label>Address</label>
+                          <input name="address" type="text" required onChange={this.handleInput}/>
+                        </li>
+                        <li>
+                          <button className="button-primary" type="submit" >Checkout</button>
+                        </li>
+                      </ul>
+                    </form>
+                  </div>
+                </Fade>
+
               )}
             </div>
           )}
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,30 +1,33 @@
 import React, { Component } from 'react'
 import formatCurrency from '../util'
+import Fade from 'react-reveal/Fade';
 
 export default class Products extends Component {
   render() {
     return (
       <div>
-        <ul className="products">
-          {this.props.products.map(product => (
-            <li key={product._id} >
-              <div className="product">
-                <a href={"#" + product._id} >
-                  <img src={product.image} alt={product.title} />
-                  <p>
-                    {product.title}
-                  </p>
-                </a>
-                <div className="product-price">
-                  <div>{ formatCurrency(product.price) }</div>
-                  <button onClick={() => this.props.addToCart(product)} className="button primary">
-                    Add to Cart
-                  </button>
+        <Fade bottom cascade={true}>
+          <ul className="products">
+            {this.props.products.map(product => (
+              <li key={product._id} >
+                <div className="product">
+                  <a href={"#" + product._id} >
+                    <img src={product.image} alt={product.title} />
+                    <p>
+                      {product.title}
+                    </p>
+                  </a>
+                  <div className="product-price">
+                    <div>{ formatCurrency(product.price) }</div>
+                    <button onClick={() => this.props.addToCart(product)} className="button primary">
+                      Add to Cart
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        </Fade>
       </div>
     )
   }
