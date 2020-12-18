cc156f66b9434690c8a3615f6499393f743d2d99
aromvergara <aromvergara@gmail.com>
2020-12-18 18:57:12 -0500

    adding animation and modal

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -2,28 +2,57 @@ import React from 'react';
 import data from "./data.json";
 import Products from "./components/Products";
 import Filter from './components/Filter';
+import Cart from './components/Cart';
 
 class App extends React.Component {
 constructor(){
   super();
   this.state = {
     products: data.products,
+    cartItems: localStorage.getItem("cartItems") 
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [],
     size: "",
     sort: "",
   };
 }
+createOrder = (order) =>{
+alert("Need to save order for " + order.name);
+}
+removeFromCart = (product) => {
+  const cartItems = this.state.cartItems.slice();
+  this.setState({cartItems: cartItems.filter((x)=>x._id !== product._id),
+  });
+  localStorage.setItem("cartItems", JSON.stringify(cartItems.filter((x)=>x._id !== product._id)));
+};
+addToCart =(product) => {
+const cartItems = this.state.cartItems.slice();
+let alreadyInCart = false;
+cartItems.forEach((item) =>{
+  if(item._id === product._id){
+    item.count++;
+    alreadyInCart = true;
+  }
+});
+if(!alreadyInCart){
+  cartItems.push({ ...product, count: 1});
+}
+this.setState({cartItems});
+localStorage.setItem("cartItems", JSON.stringify(cartItems));
+};
+
 sortProducts =(event) =>{
   const sort = event.target.value;
 
 this.setState((state) => ({
   
   sort: sort,
-  products: this.state.products.slice().sort((a,b) => (
-    sort==="lowest"?
-    ((a.price > b.price)? 1:-1):
-  sort === "highest"?
-  ((a.price < b.price)? 1:-1):
-  ((a._id < b._id)? 1:-1)  
+  products: this.state.products.slice().sort((a, b) => (
+    sort === "lowest" ?
+      ((a.price > b.price) ? 1 : -1) :
+      sort === "highest" ?
+        ((a.price < b.price) ? 1 : -1) :
+        ((a._id < b._id) ? 1 : -1)
   ))
 })); 
 };
@@ -54,10 +83,13 @@ render(){
       filterProducts = {this.filterProducts}
       sortProducts = {this.sortProducts}
       ></Filter>
-<Products products = {this.state.products}></Products>
+<Products products = {this.state.products} addToCart={this.addToCart}></Products>
     </div>
     <div className="sidebar">
-Cart Items
+<Cart cartItems={this.state.cartItems} 
+removeFromCart={this.removeFromCart}
+createOrder={this.createOrder}
+/>
     </div>
   </div>
 </main>
