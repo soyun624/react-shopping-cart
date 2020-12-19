19c8202c2b942208e52479ca08a821c9b4ba5eb1
Ariel Cardoso <arielvix@gmail.com>
2020-12-18 22:31:05 -0300

    Add redux to cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,11 @@
 import React from "react";
+import Products from "./components/Products";
+import Filter from "./components/Filter";
+import Cart from "./components/Cart";
 import store from "./store";
 import { Provider } from "react-redux";
-import Cart from "./components/Cart";
-import Filter from "./components/Filter";
-import Products from "./components/Products";
-
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
-    }
-  }
-
-  addToCart = (product) => {
-    const cartItems = this.state.cartItems.slice();
-    let alreadyInCart = false;
-    cartItems.forEach(item => {
-      if (item._id === product._id) {
-        item.count++;
-        alreadyInCart = true;
-      }
-    });
-    if (!alreadyInCart) {
-        cartItems.push({...product, count: 1});
-    }
-    this.setState({cartItems});
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  }
-
-  removeFromCart = (product) => {
-    const cartItems = this.state.cartItems.slice().filter((x) => x._id !== product._id);
-    this.setState({
-      cartItems: cartItems,
-    });
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  }
-
-  createOrder = (order) => {
-    alert('Need to save order for ' + order.name);
-  }
-
   render() {
     return (
       <Provider store={store}>
@@ -52,11 +16,11 @@ class App extends React.Component {
           <main>
             <div className="content">
               <div className="main">
-                <Filter />
-                <Products addToCart={this.addToCart} />
+                <Filter></Filter>
+                <Products></Products>
               </div>
               <div className="sidebar">
-                <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart} createOrder={this.createOrder} />
+                <Cart />
               </div>
             </div>
           </main>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
-import formatCurrency from '../util'
-import Fade from 'react-reveal/Fade';
-import Zoom from 'react-reveal/Zoom';
-import Modal from 'react-modal';
-import { connect } from 'react-redux';
-import { fetchProducts } from '../actions/productActions';
+import React, { Component } from "react";
+import formatCurrency from "../util";
+import Fade from "react-reveal/Fade";
+import Modal from "react-modal";
+import Zoom from "react-reveal/Zoom";
+import { connect } from "react-redux";
+import { fetchProducts } from "../actions/productActions";
+import { addToCart } from "../actions/cartActions";
 
 class Products extends Component {
   constructor(props) {
@@ -17,86 +18,92 @@ class Products extends Component {
     this.props.fetchProducts();
   }
   openModal = (product) => {
-    this.setState({
-      product
-    });
-  }
+    this.setState({ product });
+  };
   closeModal = () => {
-    this.setState({
-      product: null
-    });
-  }
+    this.setState({ product: null });
+  };
   render() {
-    const {product} = this.state;
+    const { product } = this.state;
     return (
       <div>
-        <Fade bottom cascade >
-          {
-            !this.props.products ? 
-            ( <div>Loading...</div> ) : 
-            (
-              <ul className="products">
-                {this.props.products.map(product => (
-                  <li key={product._id} >
-                    <div className="product">
-                      <a href={"#" + product._id} onClick={() => this.openModal(product)} >
-                        <img src={product.image} alt={product.title} />
-                        <p>
-                          {product.title}
-                        </p>
-                      </a>
-                      <div className="product-price">
-                        <div>{ formatCurrency(product.price) }</div>
-                        <button onClick={() => this.props.addToCart(product)} className="button primary">
-                          Add to Cart
-                        </button>
-                      </div>
+        <Fade bottom cascade>
+          {!this.props.products ? (
+            <div>Loading...</div>
+          ) : (
+            <ul className="products">
+              {this.props.products.map((product) => (
+                <li key={product._id}>
+                  <div className="product">
+                    <a
+                      href={"#" + product._id}
+                      onClick={() => this.openModal(product)}
+                    >
+                      <img src={product.image} alt={product.title}></img>
+                      <p>{product.title}</p>
+                    </a>
+                    <div className="product-price">
+                      <div>{formatCurrency(product.price)}</div>
+                      <button
+                        onClick={() => this.props.addToCart(product)}
+                        className="button primary"
+                      >
+                        Add To Cart
+                      </button>
                     </div>
-                  </li>
-                ))}
-              </ul>
-            )
-          }
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
         </Fade>
-        {
-          product && (
-            <Modal isOpen={true} onRequestClose={this.closeModal}  >
-              <Zoom>
-                <button className='close-modal' onClick={this.closeModal}>x</button>
-                <div className="product-details">
-                  <img src={product.image} alt={product.title} />
-                  <div className="product-details-description">
-                    <p>
-                      <strong>{product.title}</strong>
-                    </p>
-                    <p>{product.description}</p>
-                    <p>
-                      Available sizes:{" "}
-                      {product.availableSizes.map(x=>(
-                        <span>
-                          {" "}
-                          <button className='button'>{x}</button>
-                        </span>
-                      ))}
-                    </p>
-                    <p className="product-price">
-                      <div>
-                        {formatCurrency(product.price)}
-                        <button className="button primary" onClick={() => {
-                          this.props.addToCart(product);
-                          this.closeModal();
-                          }} >Add to cart</button>
-                      </div>
-                    </p>
+        {product && (
+          <Modal isOpen={true} onRequestClose={this.closeModal}>
+            <Zoom>
+              <button className="close-modal" onClick={this.closeModal}>
+                x
+              </button>
+              <div className="product-details">
+                <img src={product.image} alt={product.title}></img>
+                <div className="product-details-description">
+                  <p>
+                    <strong>{product.title}</strong>
+                  </p>
+                  <p>{product.description}</p>
+                  <p>
+                    Avaiable Sizes:{" "}
+                    {product.availableSizes.map((x) => (
+                      <span>
+                        {" "}
+                        <button className="button">{x}</button>
+                      </span>
+                    ))}
+                  </p>
+                  <div className="product-price">
+                    <div>{formatCurrency(product.price)}</div>
+                    <button
+                      className="button primary"
+                      onClick={() => {
+                        this.props.addToCart(product);
+                        this.closeModal();
+                      }}
+                    >
+                      Add To Cart
+                    </button>
                   </div>
                 </div>
-              </Zoom>
-            </Modal>
-          )
-        }
+              </div>
+            </Zoom>
+          </Modal>
+        )}
       </div>
-    )
+    );
   }
 }
-
-export default connect((state) => ({products:state.products.items}), {fetchProducts})(Products)
+export default connect(
+  (state) => ({ products: state.products.filteredItems }),
+  {
+    fetchProducts,
+    addToCart,
+  }
+)(Products);
