c2cb00e4b61c1a258ed2dfadc3dd547c0b6f7017
Ariel Cardoso <arielvix@gmail.com>
2020-12-20 05:06:29 -0300

    Set express to the same host

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const shortid = require("shortid");
+var path = __dirname + '/server/build';
 
 const app = express();
+app.use(express.static(path));
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
   useNewUrlParser: true,
@@ -40,5 +49,17 @@ app.delete("/api/products/:id", async(req, res) => {
   res.send(deletedProduct);
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("serve at http://localhost:5000"));
+app.get('/', function (req,res) {
+  res.sendFile(path + "index.html");
+});
+
+//require("./server/routes/turorial.routes")(app);
+
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => console.log("serve at http://localhost:5000"));
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
